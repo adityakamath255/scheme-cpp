ad28a71aa041c68f5206a3d6c1203a1347bf09ef
adityakamath255 <aditya.kamath.270@gmail.com>
2026-03-22 10:10:35 +0530

    lex.hpp

---
+++ include/lex.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <string_view>
+#include <vector>
+#include <optional>
+
+struct Token {
+  enum Type {
+    LPAREN,
+    RPAREN,
+    NUMBER,
+    SYMBOL,
+    STRING,
+    TRUE,
+    FALSE,
+    CHAR,
+    DOT,
+    QUOTE,
+    BACKTICK,
+    COMMA,
+    SPLICE_COMMA,
+    PLUS_INF,
+    MINUS_INF,
+    NAN_VAL,
+    VEC_BEGIN,
+  } type;
+
+  std::string_view lexeme;
+};
+
+struct LexResult {
+  std::vector<Token> tokens;
+  std::string_view rest;
+};
+
+std::optional<LexResult> lex(std::string_view input);
