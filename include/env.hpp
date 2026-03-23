c540039c515453374dddb7bd8f3f0dd27846a739
adityakamath255 <aditya.kamath.270@gmail.com>
2026-03-23 13:11:40 +0530

    flat vector bindings for local envs

--- include/env.hpp
+++ include/env.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include "types.hpp"
-#include <unordered_map>
+#include <memory>
 
 class Env : public HeapEntity {
-private:
-  std::unordered_map<Symbol, Obj> bindings;
+  struct Bindings;
+  std::unique_ptr<Bindings> bindings;
   Env *parent;
 
 public:
   Env(Env *);
-  
+  ~Env();
+
   std::optional<Obj> lookup(Symbol name) const;
   void define(Symbol name, Obj value);
   bool set(Symbol name, Obj value);
 
   void trace(std::vector<HeapEntity *> *) const override;
 };
-
--- src/env.cpp
+++ src/env.cpp
@@ -1,44 +1,110 @@
 #include "env.hpp"
+#include <unordered_map>
 
-Env::Env(Env *parent): bindings {}, parent {parent} {}
+// global envs (parent == nullptr) use hashmaps for bindings,
+// local envs (parent != nullptr) use flat vectors instead
+
+struct Env::Bindings {
+  using VecBindings = std::vector<std::pair<Symbol, Obj>>;
+  using MapBindings = std::unordered_map<Symbol, Obj>;
+  using Value = std::variant<VecBindings, MapBindings>;
+
+  Value data;
+
+  Bindings(bool map) {
+    if (map) {
+      data = MapBindings{};
+    }
+    else {
+      data = VecBindings{};
+    }
+  }
+
+  bool is_vec() const {
+    return std::holds_alternative<VecBindings>(data);
+  }
+
+  bool is_map() const {
+    return std::holds_alternative<MapBindings>(data);
+  }
+
+  VecBindings &as_vec() { return std::get<VecBindings>(data); }
+  const VecBindings &as_vec() const { return std::get<VecBindings>(data); }
+
+  MapBindings &as_map() { return std::get<MapBindings>(data); }
+  const MapBindings &as_map() const { return std::get<MapBindings>(data); }
+};
+
+Env::Env(Env *parent):
+  bindings {std::make_unique<Bindings>(parent == nullptr)},
+  parent {parent}
+{}
+
+Env::~Env() = default;
 
 std::optional<Obj> Env::lookup(Symbol sym) const {
-  auto res = bindings.find(sym);
-  if (res != bindings.end()) {
-    return res->second;
-  }
-  else if (parent) {
-    return parent->lookup(sym);
-  }
+  if (bindings->is_map()) {
+    auto &map = bindings->as_map();
+    auto it = map.find(sym);
+    if (it != map.end()) {
+      return it->second;
+    }
+  } 
   else {
-    return std::nullopt;
+    for (const auto &[k, v] : bindings->as_vec()) {
+      if (k == sym) return v;
+    }
   }
+  return parent ? parent->lookup(sym) : std::nullopt;
 }
 
 void Env::define(Symbol sym, Obj obj) {
-  bindings.insert_or_assign(sym, obj);
+  if (bindings->is_map()) {
+    bindings->as_map().insert_or_assign(sym, obj);
+  } 
+  else {
+    bindings->as_vec().emplace_back(sym, obj);
+  }
 }
 
 bool Env::set(Symbol sym, Obj obj) {
-  auto res = bindings.find(sym);
-  if (res != bindings.end()) {
-    res->second = obj;
-    return true;
-  }
-  else if (parent) {
-    return parent->set(sym, obj);
-  }
+  if (bindings->is_map()) {
+    auto &map = bindings->as_map();
+    auto it = map.find(sym);
+    if (it != map.end()) {
+      it->second = obj;
+      return true;
+    }
+  } 
   else {
-    return false;
+    for (auto &[k, v] : bindings->as_vec()) {
+      if (k == sym) {
+        v = obj;
+        return true;
+      }
+    }
   }
+  return parent ? parent->set(sym, obj) : false;
 }
 
 void Env::trace(std::vector<HeapEntity *> *worklist) const {
-  for (const auto &[_, obj] : bindings) {
+  auto trace_obj = [&](const Obj &obj) {
     if (auto entity = obj.heap_entity()) {
       worklist->push_back(*entity);
     }
+  };
+
+  if (bindings->is_map()) {
+    for (const auto &[_, obj] : bindings->as_map()) {
+      trace_obj(obj);
+    }
+  } 
+  else {
+    for (const auto &[_, obj] : bindings->as_vec()) {
+      trace_obj(obj);
+    }
   }
+
   if (parent) {
     worklist->push_back(parent);
   }
